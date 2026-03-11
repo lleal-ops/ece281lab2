20a5edf0d68d6781a1e70fc0d05735f4d2836df9
lleal-ops <225130384+lleal-ops@users.noreply.github.com>
2026-03-11 11:31:22 -0600

    Submission

--- src/sevenseg_decoder_tb.vhd
+++ src/sevenseg_decoder_tb.vhd
@@ -42,69 +42,39 @@ architecture Behavioral of sevenseg_decoder_tb is
         );
     end component;
 
-    signal w_hex   : STD_LOGIC_VECTOR (3 downto 0) := "0000";
-    signal w_seg_n : STD_LOGIC_VECTOR (6 downto 0);
+    signal i_hex   : STD_LOGIC_VECTOR (3 downto 0) := "0000";
+    signal o_seg_n : STD_LOGIC_VECTOR (6 downto 0);
 
 begin
 
     uut: sevenseg_decoder
         port map (
-            i_hex   => w_hex,
-            i_seg_n => w_seg_n
+            i_hex   => i_hex,
+            o_seg_n => o_seg_n
         );
 
-   test_process : process
-   begin
-        w_hex <= x"0"; wait for 10 ns;
-        assert w_seg_n = "1000000" report "ERROR on 0" severity failure;
-        
-        w_hex <= x"1"; wait for 10 ns;
-        assert w_seg_n = "1111001" report "ERROR on 1" severity failure;
-        
-        w_hex <= x"2"; wait for 10 ns;
-        assert w_seg_n = "0100100" report "ERROR on 2" severity failure;
-        
-        w_hex <= x"3"; wait for 10 ns;
-        assert w_seg_n = "0110000" report "ERROR on 3" severity failure;
-        
-        w_hex <= x"4"; wait for 10 ns;
-        assert w_seg_n = "0011001" report "ERROR on 4" severity failure;
-        
-        w_hex <= x"5"; wait for 10 ns;
-        assert w_seg_n = "0010010" report "ERROR on 5" severity failure;
-        
-        w_hex <= x"6"; wait for 10 ns;
-        assert w_seg_n = "0000010" report "ERROR on 6" severity failure;
-        
-        w_hex <= x"7"; wait for 10 ns;
-        assert w_seg_n = "1111000" report "ERROR on 7" severity failure;
-        
-        w_hex <= x"8"; wait for 10 ns;
-        assert w_seg_n = "0000000" report "ERROR on 8" severity failure;
-        
-        w_hex <= x"9"; wait for 10 ns;
-        assert w_seg_n = "0010000" report "ERROR on 9" severity failure;
-        
-        w_hex <= x"A"; wait for 10 ns;
-        assert w_seg_n = "0001000" report "ERROR on A" severity failure;
-        
-        w_hex <= x"B"; wait for 10 ns;
-        assert w_seg_n = "0000011" report "ERROR on B" severity failure;
-        
-        w_hex <= x"C"; wait for 10 ns;
-        assert w_seg_n = "1000110" report "ERROR on C" severity failure;
-        
-        w_hex <= x"D"; wait for 10 ns;
-        assert w_seg_n = "0100001" report "ERROR on D" severity failure;
-        
-        w_hex <= x"E"; wait for 10 ns;
-        assert w_seg_n = "0000110" report "ERROR on E" severity failure;
-        
-        w_hex <= x"F"; wait for 10 ns;
-        assert w_seg_n = "0001110" report "ERROR on F" severity failure;
-        
+    stim_proc: process
+    begin
+        i_hex <= "0000"; wait for 10 ns;
+        i_hex <= "0001"; wait for 10 ns;
+        i_hex <= "0010"; wait for 10 ns;
+        i_hex <= "0011"; wait for 10 ns;
+        i_hex <= "0100"; wait for 10 ns;
+        i_hex <= "0101"; wait for 10 ns;
+        i_hex <= "0110"; wait for 10 ns;
+        i_hex <= "0111"; wait for 10 ns;
+        i_hex <= "1000"; wait for 10 ns;
+        i_hex <= "1001"; wait for 10 ns;
+        i_hex <= "1010"; wait for 10 ns;
+        i_hex <= "1011"; wait for 10 ns;
+        i_hex <= "1100"; wait for 10 ns;
+        i_hex <= "1101"; wait for 10 ns;
+        i_hex <= "1110"; wait for 10 ns;
+        i_hex <= "1111"; wait for 10 ns;
         wait;
-        
-     end process;
+    end process;
+
+
+
 
 end Behavioral;
